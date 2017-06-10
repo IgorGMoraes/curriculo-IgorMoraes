5a595455f359be01490c4bc882fd7b458adf070c
Igor Gomes Moraes <igorgomesmoraes@gmail.com>
2017-06-10 14:28:28 -0300

    v 0.2
    -Normal scroll on mobile

--- js/main.js
+++ js/main.js
@@ -12,24 +12,12 @@ $(function(){
 
 			$(".nav").find("a[href=\"#" + ref + "\"]").addClass("active");
 		},
-
-		//Faz com que os links na navbar movam para as sections quando clicados 
-		afterRender:function() {
-			// var nav = "<ul class=\"nav\">";
-			// var activeClass = "";
-			// $(".panel").each(function(i) {
-			// 	activeClass = "";
-			// 	if(i===0) {
-			// 		activeClass = "active";
-			// 	}
-			// 	nav += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name");
-			// });
-			$(".nav a").on("click",$.scrollify.move);
-			
-		},
-
 	});
 });
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+	$.scrollify.disable();
+}
 
 $(function () {
   $(document).scroll(function () {

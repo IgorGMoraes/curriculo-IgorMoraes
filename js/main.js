731634894cef42d02063dec966a1489ec2d11da1
Igor Gomes Moraes <igorgomesmoraes@gmail.com>
2017-06-16 23:14:10 -0300

    v1.0

--- js/main.js
+++ js/main.js
@@ -12,11 +12,28 @@ $(function(){
 
 			$(".nav").find("a[href=\"#" + ref + "\"]").addClass("active");
 		},
+		afterRender:function() {
+			var nav = "<ul class=\"nav\">";
+			var activeClass = "";
+			$(".panel").each(function(i) {
+				activeClass = "";
+				if(i===0) {
+					activeClass = "active";
+				}
+				nav += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name");
+			});
+
+			nav += "</ul>";
+
+			$(".home").append(nav);
+			$(".nav a").on("click",$.scrollify.move);
+		}
 	});
 });
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
 	$.scrollify.disable();
+
 }
 
 $(function () {
